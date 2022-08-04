3ee66aef9fce8978e3152a2ff562c23f46c1f387
Eythor Ivarsson <eythori@ei.lan>
2022-08-04 21:04:44 +0000

    Refactoring of code adding re and logging

--- unbound-blocker.py
+++ unbound-blocker.py
@@ -1,69 +1,93 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-# EgonRassi Unbound Blocker from list
+""" 
+Unbound Blocker
+Middleware to read a public list of domains to block in the
+Unbound resolver without needing a restart.
+Utilizes the unbound-control interface.
 
-import argparse,requests,validators
+"""
 
-#Define const
-currentlist=[]
-freshlist=[]
-remove_bulk = []
-add_bulk = []
-
-# Parse args
-parser = argparse.ArgumentParser(description='Block domains in Unbound through a public list')
-parser.add_argument('--input', dest='input',action='append',nargs='+', required=True,help='URL to the public list for domains to block - This can have multiple instances')
-parser.add_argument('--unboundhost', dest='ubhost', default='localhost', help='Unbound Control server address or hostname')
-parser.add_argument('--unboundport', dest='ubport', default=8953, type=int, help='Unbound Control server port')
-parser.add_argument('--unboundservercert', dest='ubservercert', default='/etc/unbound/unbound_server.pem', help='Unbound Control Server Certificate')
-parser.add_argument('--unboundclientcert', dest='ubclientcert', default='/etc/unbound/unbound_control.pem', help='Unbound Control Client Certificate')
-parser.add_argument('--unboundclientkey', dest='ubclientkey', default='/etc/unbound/unbound_control.key', help='Unbound Control Client Certificate key')
-parser.add_argument('--commit', dest='commit', action='store_true', required=False,help='Commit changes if any')
-args = parser.parse_args()
-
-# Connect to Unbound Control
+import argparse, requests, validators, logging, sys, re
 from unbound_console import RemoteControl
-rc = RemoteControl(host=args.ubhost, port=args.ubport,
-                   server_cert = args.ubservercert,
-                   client_cert= args.ubclientcert,
-                   client_key= args.ubclientkey)
 
-# Get the input - expect multiple lists
-for urlinput in args.input:
- try:
-    r = requests.get(urlinput[0])
- except requests.exceptions.RequestException as e:
-    raise SystemExit(e)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
- for bllist in r.text.split("\n"):
-  #Verify the domain is valid, and add it to the list if it does
-  if validators.domain(bllist):
-    freshlist.append(bllist)
+def main(args):
+  #Define const
+  currentlist=[]
+  freshlist=[]
+  remove_bulk = []
+  add_bulk = []
 
-# Get the current state
-o = rc.send_command(cmd="list_local_zones")
-for localzone in o.split("\n"):
+  # Get a fresh list of domains to block - expect multiple input arguments
+  for urlinput in args.input:
+    try:
+      r = requests.get(urlinput[0])
+    except requests.exceptions.RequestException as e:
+      raise SystemExit(e)
+
+    for blocklist in r.text.split("\n"):
+      if not validators.domain(blocklist):
+        continue
+      if args.ignore:
+        for ignlist in args.ignore:
+          if re.search(ignlist[0], blocklist):
+            break
+        else:
+          freshlist.append(blocklist)
+      else:
+        freshlist.append(blocklist)
+
+  # Connect to Unbound Control
+  rc = RemoteControl(host=args.ubhost, port=args.ubport,
+                    server_cert = args.ubservercert,
+                    client_cert= args.ubclientcert,
+                    client_key= args.ubclientkey)
+
+  # Get the current state
+  o = rc.send_command(cmd="list_local_zones")
+  for localzone in o.split("\n"):
     if localzone.find("always_nxdomain")!=-1:
-     currentlist.append((localzone.split(". ")[0]))
+      currentlist.append((localzone.split(". ")[0]))
 
-# Run diffs
-difference_toremove = list(set(currentlist) - set(freshlist))
-difference_toadd = list(set(freshlist) - set(currentlist))
-changes=len(difference_toremove) + len(difference_toadd)
-if changes == 0:
-    print("No differences detected")
-else:
-    print("Being blocked but not in list: " + str(difference_toremove))
-    print("Currently not blocked: " + str(difference_toadd))
+  # Run diffs - exit if no changes are needed
+  difference_toremove = list(set(currentlist) - set(freshlist))
+  difference_toadd = list(set(freshlist) - set(currentlist))
+  changes=len(difference_toremove) + len(difference_toadd)
+  if changes == 0:
+      logging.info("No differences detected. No need to continue - exiting.")
+      sys.exit(0)
+  else:
+      logging.info("Being blocked but not in list: " + str(difference_toremove))
+      logging.info("Currently not blocked: " + str(difference_toadd))
 
-# Commit changes if argument commit is true and changes need to be made
-if args.commit == True and changes != 0:
- for remove_zone in difference_toremove:
-   remove_bulk.append( remove_zone )
+  # Commit changes if argument commit is true and changes need to be made
+  if args.commit == True and changes != 0:
+    #Remove missing zones
+    for remove_zone in difference_toremove:
+      remove_bulk.append( remove_zone )
+    # Add missing zones
+    for add_zone in difference_toadd:
+      add_bulk.append( add_zone + " always_nxdomain")
+    logging.info("Committing changes")
+    # Commit changes to Unbound
+    o = rc.send_command(cmd="local_zones_remove", data_list=remove_bulk)
+    o = rc.send_command(cmd="local_zones", data_list=add_bulk)
 
- for add_zone in difference_toadd:
-   add_bulk.append( add_zone + " always_nxdomain")
- print("Committing changes")
+if __name__ == '__main__':
+  # Parse args
+  parser = argparse.ArgumentParser(description='Block domains in Unbound through a public list')
+  parser.add_argument('--input', dest='input', action='append', nargs='+', required=True, help='URL to the public list for domains to block - Multiple arguments allowed')
+  parser.add_argument('--unboundhost', dest='ubhost', default='localhost', help='Unbound Control server address or hostname')
+  parser.add_argument('--unboundport', dest='ubport', default=8953, type=int, help='Unbound Control server port')
+  parser.add_argument('--unboundservercert', dest='ubservercert', default='/etc/unbound/unbound_server.pem', help='Unbound Control Server Certificate')
+  parser.add_argument('--unboundclientcert', dest='ubclientcert', default='/etc/unbound/unbound_control.pem', help='Unbound Control Client Certificate')
+  parser.add_argument('--unboundclientkey', dest='ubclientkey', default='/etc/unbound/unbound_control.key', help='Unbound Control Client Certificate key')
+  parser.add_argument('--commit', dest='commit', action='store_true', required=False, help='Commit changes if any')
+  parser.add_argument('--ignore', dest='ignore', action='append', nargs='+', required=False, help='Ignore domains - Regex syntax')
+  args = parser.parse_args()
 
- o = rc.send_command(cmd="local_zones_remove", data_list=remove_bulk)
- o = rc.send_command(cmd="local_zones", data_list=add_bulk)
+  main(args)
+  sys.exit(0)
